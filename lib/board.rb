5fdf00c254027fda6c2b5a704046edba039ea4b0
chris <christanr00@gmail.com>
2023-02-12 15:41:33 -0500

    board place_piece and spec

---
+++ lib/board.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Handles board logic
+class Board
+  def initialize
+    @board = Array.new(7) { Array.new(6) }
+  end
+
+  def place_piece(piece, col)
+    @board[col].each_with_index do |row, idx|
+      if row.nil?
+        @board[col][idx] = piece
+        break
+      end
+    end
+  end
+end
